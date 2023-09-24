91b6d3ebf291032843128bb5e3fa2f9572f9c559
benjamina107 <142339150+benjamina107@users.noreply.github.com>
2023-09-24 12:13:24 -0600

    Update Stack.java

--- src/Stack.java
+++ src/Stack.java
@@ -1,1 +1,29 @@
+public class Stack {
+
+    public void push(int element) {
+
+    }
+
+    public int pop() {
+
+    }
+
+    public int peek() {
+
+    }
+
+    public int size() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+  
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+
 
