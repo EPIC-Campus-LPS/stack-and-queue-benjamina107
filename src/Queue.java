a4674b6d878cea907a29c0df9c22cbdb67195566
benjamina107 <142339150+benjamina107@users.noreply.github.com>
2023-09-28 17:16:31 -0600

    Update Queue.java

--- src/Queue.java
+++ src/Queue.java
@@ -1,1 +1,53 @@
+import java.util.NoSuchElementException;
+public class Queue {
+
+
+    private int[] q;
+
+    private int s = 0;
+
+    public Queue() {}
+
+  
+    public void add(int element) {
+      int[] temp = new int[s + 1]
+      if (s > 0) {
+        for (int i = 1; i < s; i++) {
+          temp[i] = q[i-1];
+        }
+        q = temp;
+        q[0] = element;
+        s++;
+      } else {
+        q[0] = element;
+        s++;
+      }
+      
+    }
+  
+    public int remove() {
+      if (s == 0) {throw new NoSuchElementException();}
+      int tmp = q[s-1];
+      int[] temp = new int[s-1];
+      for(int i = 0; i < (s-1); i++) {
+        temp[i] = q[i];
+      }
+      q = temp;
+      s--;
+      return tmp;
+    }
+  
+    public int peek() {
+       if (s == 0) {throw new NoSuchElementException();}
+       return q[s-1];
+    }
+  
+    public int size() {
+      return s;
+    }
+  
+    public boolean isEmpty() {
+      return (size <= 0);
+    }
+}
 
